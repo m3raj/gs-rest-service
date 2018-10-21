a919e3a02921233ebeeee672a6830d64b80b8e99
m3raj <44254744+m3raj@users.noreply.github.com>
2018-10-21 20:16:52 +0530

    Update WordController.java
    TechM Demo

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -5,4 +5,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WordController {
     // TODO Implement the /words/{word} endpoint
+    
+    @RequestMapping("/words/{word}")
+	public Palindrome getPaindrome(@RequestParam(value="word", required=true) String word) {
+		int n = word.length();
+		String a = "" , b = "";
+		boolean res = false;
+		Palindrome p = new Palindrome();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            b = b + a.charAt(i);
+        }
+        p.setWord(word);
+        if(a.equalsIgnoreCase(b))
+        {
+        	res = true;
+        }
+        else
+        {
+        	res = false;
+        }
+        p.setPalindrome(res);
+        p.setAnagramOfPalindrome(false);
+        
+        return p;
+	}
+    
 }
